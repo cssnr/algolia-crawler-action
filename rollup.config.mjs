053756af967779475a2fc311045c980c64c086f0
Shane <6071159+smashedr@users.noreply.github.com>
2026-06-04 09:36:17 -0700

    Update TypeScript, Release and Build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,11 +5,11 @@ import typescript from '@rollup/plugin-typescript'
 
 // noinspection JSUnusedGlobalSymbols
 export default {
-    input: 'src/index.ts',
-    output: {
-        esModule: true,
-        file: 'dist/index.js',
-        format: 'esm',
-    },
-    plugins: [commonjs(), json(), nodeResolve({ preferBuiltins: true }), typescript()],
+  input: 'src/index.ts',
+  output: {
+    esModule: true,
+    file: 'dist/index.js',
+    format: 'esm',
+  },
+  plugins: [commonjs(), json(), nodeResolve({ preferBuiltins: true }), typescript()],
 }
